a049b771632a76d3a0c3d9ba9062dbdab608437d
Junseong Kim <codertimo@gmail.com>
2021-08-10 17:15:36 +0000

    feat: https request 지원하기

--- ngrok-server.go
+++ ngrok-server.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"bytes"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"net/http"
+	"net/http/httputil"
 )
 
 var remoteControlAddr *string = flag.String("c", "localhost:5000", "remote control address")
 var remoteDataAddr *string = flag.String("d", "localhost:5001", "remote data address")
 
-var localAddr *string = flag.String("l", "localhost:4000", "local address")
+var localAddr *string = flag.String("l", ":443", "local address")
+var tlsCertFilePath *string = flag.String("tc", "/etc/letsencrypt/live/scatternel.ml/fullchain.pem", "certificate file")
+var tlsKeyFilePath *string = flag.String("tk", "/etc/letsencrypt/live/scatternel.ml/privkey.pem", "privated key")
 
 type signal = struct{}
 
@@ -22,21 +28,20 @@ func main() {
 	log.Println("Local Address:", *localAddr)
 
 	dataConnChan := make(chan net.Conn)
-	go listenTCP(remoteDataAddr, func(dataConn net.Conn) {
-		log.Printf("Data connection open: %s\n", dataConn.LocalAddr())
-		dataConnChan <- dataConn
-	})
-
 	var remoteConn *net.Conn
 
-	go listenTCP(localAddr, func(localConn net.Conn) {
-		log.Printf("Local request: %s\n", localConn.LocalAddr())
-
-		/* when developer request */
-		defer localConn.Close()
-
-		_, err := (*remoteConn).Write([]byte("o"))
+	http.HandleFunc("/", func(responseWriter http.ResponseWriter, request *http.Request) {
+		// request.Proto = "HTTP/1.1"
+		requestDump, err := httputil.DumpRequest(request, true)
+		log.Println(string(requestDump))
+		requestReader := bytes.NewReader(requestDump)
 		if err != nil {
+			http.Error(responseWriter, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+
+		_, err2 := (*remoteConn).Write([]byte("o"))
+		if err2 != nil {
 			panic(err)
 		}
 
@@ -44,12 +49,18 @@ func main() {
 		defer dataConn.Close()
 
 		closer := make(chan signal)
-		go copy(closer, dataConn, localConn)
-		go copy(closer, localConn, dataConn)
+		go copy(closer, dataConn, requestReader)
+		go copy(closer, responseWriter, dataConn)
 		<-closer
+		log.Printf("Data connection close: %s\n", dataConn.LocalAddr())
 	})
 
-	listenTCP(remoteControlAddr, func(newRemoteConn net.Conn) {
+	go listenTCP(remoteDataAddr, func(dataConn net.Conn) {
+		log.Printf("Data connection open: %s\n", dataConn.LocalAddr())
+		dataConnChan <- dataConn
+	})
+
+	go listenTCP(remoteControlAddr, func(newRemoteConn net.Conn) {
 		if remoteConn != nil {
 			(*remoteConn).Close()
 		}
@@ -58,6 +69,9 @@ func main() {
 		/* when user request */
 		log.Printf("Control connection open: %s\n", newRemoteConn.LocalAddr())
 	})
+
+	err := http.ListenAndServeTLS(*localAddr, *tlsCertFilePath, *tlsKeyFilePath, nil)
+	log.Fatal(err)
 }
 
 func copy(closer chan signal, dst io.Writer, src io.Reader) {
